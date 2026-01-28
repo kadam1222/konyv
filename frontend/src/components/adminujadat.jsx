4dee6b0fcbed859a9b48676d1b398c006a54480c
kadam1222 <k.adam12220@gmail.com>
2026-01-28 13:52:47 +0100

    több input

--- frontend/src/components/adminujadat.jsx
+++ frontend/src/components/adminujadat.jsx
@@ -6,6 +6,12 @@ export default function AdminAdatFelvetel({ accessToken, onSiker }) {
     const [borito, setBorito] = useState("");
     const [kiado, setKiado] = useState("");
     const [szerzo, setSzerzo] = useState("");
+    const [fordito, setFordito] = useState("");
+    const [illusztracio, setIllusztracio] = useState("");
+    const [illusztrator, setIllusztrator] = useState("");
+    const [nyelv, setNyelv] = useState("");
+    const [kategoria, setKategoria] = useState("");
+    const [fokat, setFokat] = useState("");
 
     // Közös mentő függvény
     const mentés = async (kulcs, ertek, setter) => {
@@ -54,6 +60,56 @@ export default function AdminAdatFelvetel({ accessToken, onSiker }) {
                     onChange={(e) => setSzerzo(e.target.value)} 
                 />
                 <button onClick={() => mentés("szerzo", szerzo, setSzerzo)}>Hozzáad</button>
+            </div>
+
+            <div style={{ marginBottom: "15px" }}>
+                <label>Új fordító felvétele: </label>
+                <input 
+                    placeholder="pl. Acsai Roland" 
+                    value={fordito}
+                    onChange={(e) => setFordito(e.target.value)} 
+                />
+                <button onClick={() => mentés("fordito", fordito, setFordito)}>Hozzáad</button>
+            </div>
+
+            <div style={{ marginBottom: "15px" }}>
+                <label>Új illusztráció felvétele: </label>
+                <input 
+                    placeholder="pl. Színes Képekkel, Fotókkal" 
+                    value={illusztracio}
+                    onChange={(e) => setIllusztracio(e.target.value)} 
+                />
+                <button onClick={() => mentés("illusztracio", illusztracio, setIllusztracio)}>Hozzáad</button>
+            </div>
+
+            <div style={{ marginBottom: "15px" }}>
+                <label>Új Illusztrátor felvétele: </label>
+                <input 
+                    placeholder="pl. Alice Oseman" 
+                    value={illusztrator}
+                    onChange={(e) => setIllusztrator(e.target.value)} 
+                />
+                <button onClick={() => mentés("illusztrator", illusztrator, setIllusztrator)}>Hozzáad</button>
+            </div>
+
+            <div style={{ marginBottom: "15px" }}>
+                <label>Új nyelv felvétele: </label>
+                <input 
+                    placeholder="pl. Magyar" 
+                    value={nyelv}
+                    onChange={(e) => setNyelv(e.target.value)} 
+                />
+                <button onClick={() => mentés("nyelv", nyelv, setNyelv)}>Hozzáad</button>
+            </div>
+
+            <div style={{ marginBottom: "15px" }}>
+                <label>Új kategória felvétele: </label>
+                <input 
+                    placeholder="pl. Magyar" 
+                    value={nyelv}
+                    onChange={(e) => setNyelv(e.target.value)} 
+                />
+                <button onClick={() => mentés("nyelv", nyelv, setNyelv)}>Hozzáad</button>
             </div>
         </div>
     );
